ff43c1527ca443bb89e4e91dd9fbc49aa7c9af10
Divyanshu0476 <divyanshu0476@gmail.com>
2026-04-28 17:38:56 +0530

    Create Count.sql

---
+++ Window_funtion/Count.sql
@@ -0,0 +1,22 @@
+-- Count Fn with duplicates
+-- Duplicates within same department salary
+
+    select
+      select  distinct salary, dup_sal from (
+    select 
+    	employee_name,
+        department_id,
+        salary,
+        count(*) over(partition by department_id,salary) dup_sal
+      from employees) t
+       where dup_sal>1
+  ; 
+| employee | dept | salary |
+| -------- | ---- | ------ |
+| A        | 10   | 50000  |
+| B        | 10   | 60000  |
+| C        | 10   | 50000  |
+
+Partitions formed:
+(10, 50000) → A, C → count = 2
+(10, 60000) → B → count = 1
